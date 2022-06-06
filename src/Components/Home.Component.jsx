247e2e5c020575df206d52e8e8918bcaa38e88f7
Rakib Hosen <rakib.151045@gmail.com>
2022-06-06 21:25:26 +0600

    show offer on home page

--- src/Components/Home.Component.jsx
+++ src/Components/Home.Component.jsx
@@ -1,6 +1,42 @@
+import Navbar from "../Components/Common/Navbar.Component";
+import {list } from "../service/offerService";
+import { useState,useEffect } from "react";
 const Home = () => {
+  const [offerList, setofferList] = useState([]);
+  useEffect(() => {
+    (async () => {
+      let response = await list();
+      let { data: responseData } = response;
+      if (responseData.status && responseData.status == "ok") {
+        setofferList(responseData.offers);
+      }
+    })();
+  }, []);
     return ( 
-        <h1>This is Home</h1>
+      <>
+        <Navbar />
+        <h2>Offer List</h2>
+         <div className="row">
+         {
+            offerList.length>0?(
+              offerList.map(e=>{
+              return (  <div className="col-md-4">
+              <div className="card">
+                <div className="card-header bg-info">
+                  <div>Offer Name - {e.name}</div>
+                  <div>Percentage - {e.percentage}%</div>
+                </div>
+                <div className="card-body">
+                  <img src={e.offer_url} width="90px" />
+                </div>
+                <div className="card-footer">Partner-  {e.partner.name}</div>
+              </div>
+            </div>)
+              })
+            ):"No Record Found"
+          }
+         </div>
+      </>
      );
 }
  
