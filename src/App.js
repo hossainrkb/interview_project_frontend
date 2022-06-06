3bfe8c7bacb090a44865e6af319df61fbef632de
Rakib Hosen <rakib.151045@gmail.com>
2022-06-06 21:24:46 +0600

    imp context api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,128 @@
-import React,{Component} from "react";
-import { BrowserRouter, Routes, Navigate, Route  } from "react-router-dom";
-import Navbar from "./Components/Common/Navbar.Component";
-import Login from './Components/Login.Component'
-import PartnerAdd from './Components/PartnerAdd.Component'
-import Offer from './Components/Offer.Component'
-import PartnerEdit from './Components/PartnerEdit.Component'
-import OfferAdd from './Components/OfferAdd.Component'
-import Partner from './Components/Partner.Component'
-import OfferEdit from './Components/OfferEdit.Component'
-import Home from './Components/Home.Component'
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-      <BrowserRouter>
-       <Navbar />
-       <Routes>
-         <Route path="/home" element={<Home/>} />
-         <Route exact path="/login" element={<Login />} />
-         <Route exact path="/partner" element={<Partner />} />
-         <Route exact path="/partner/create" element={<PartnerAdd />} />
-         <Route exact path="/partner/:id/edit" element={<PartnerEdit />} />
-         <Route exact path="/partner/:id/offers" element={<Offer />} />
-         <Route exact path="/partner/:id/offers/create" element={<OfferAdd />} />
-         <Route exact path="/offer/:id/edit" element={<OfferEdit />} />
-         {/* <Route exact path="/" render={() => alert("Its Coming Home")} />
-         <Navigate to="/not-found"/> */}
-       </Routes>
-     </BrowserRouter>
- </div>
-    )
-  }
-}
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Login from "./Components/Login.Component";
+import Register from "./Components/Register.Component";
+import PartnerAdd from "./Components/PartnerAdd.Component";
+import Offer from "./Components/Offer.Component";
+import PartnerEdit from "./Components/PartnerEdit.Component";
+import OfferAdd from "./Components/OfferAdd.Component";
+import Partner from "./Components/Partner.Component";
+import OfferEdit from "./Components/OfferEdit.Component";
+import Home from "./Components/Home.Component";
+import ProtectedRoute from "./Components/Common/ProtectedRoute.Component";
+import { getCurrentUser,logout } from "./service/userService";
+import UserContext from "./Context/UserContext";
+import { useNavigate } from "react-router-dom";
+function App() {
+  const [user, setUser] = useState({});
+  const navigation = useNavigate();
+  useEffect(() => {
+    if (!Object.keys(user).length) {
+      (async () => {
+        let userResponse = await getCurrentUser();
+        let { data: userData } = userResponse;
+        if (userData.status && userData.status == "ok") {
+          setUser(userData.data);
+        }
+      })();
+    }
+  });
 
+  const handleLogout = async ()=>{
+    await logout();
+    setUser({});
+    navigation("/login");
+  }
+  return (
+    <div className="container">
+     
+        <UserContext.Provider value={{ user,handleLogout }}>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+
+            <Route
+              exact
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              exact
+              path="/home"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              exact
+              path="/partner"
+              element={
+                user.type == "admin" ? (
+                  <ProtectedRoute>
+                    <Partner />
+                  </ProtectedRoute>
+                ) : (
+                  <Navigate to="/" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/partner/create"
+              element={
+                <ProtectedRoute>
+                  <PartnerAdd />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              exact
+              path="/partner/:id/edit"
+              element={
+                <ProtectedRoute>
+                  <PartnerEdit />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              exact
+              path="/partner/:id/offers"
+              element={
+                <ProtectedRoute>
+                  <Offer />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              exact
+              path="/partner/:id/offers/create"
+              element={
+                <ProtectedRoute>
+                  <OfferAdd />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              exact
+              path="/offer/:id/edit"
+              element={
+                <ProtectedRoute>
+                  <OfferEdit />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </UserContext.Provider>
+    </div>
+  );
+}
 
 export default App;
--- src/Components/Common/Navbar.Component.jsx
+++ src/Components/Common/Navbar.Component.jsx
@@ -1,18 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { getCurrentUser } from "../../service/userService";
+import { getCurrentUser, logout } from "../../service/userService";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../Context/UserContext";
 function Navbar() {
-  const [user, setUser] = useState({});
-  useEffect(() => {
-    (async () => {
-      let userResponse = await getCurrentUser();
-      let { data: userData } = userResponse;
-      if (userData.status && userData.status == "ok") {
-        setUser(userData.data);
-      }
-    })();
-  }, []);
+  const navigation = useNavigate();
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -32,21 +25,36 @@ function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/partner">
-                Partners
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                className="nav-link active"
-                aria-current="page"
-                to="/offers"
-              >
-                Offers
-              </Link>
-            </li>
+            <UserContext.Consumer>
+              {({ user }) => {
+                return user.type && user.type == "admin" ? (
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="/partner"
+                    >
+                      Partners
+                    </Link>
+                  </li>
+                ) : (
+                  ""
+                );
+              }}
+            </UserContext.Consumer>
           </ul>
+          <UserContext.Consumer>
+            {({ user, handleLogout }) => {
+              return (
+                <>
+                  <h3>Name : {user.name}</h3>
+                  <button className="btn bg-danger" onClick={handleLogout}>
+                    Logout
+                  </button>
+                </>
+              );
+            }}
+          </UserContext.Consumer>
         </div>
       </div>
     </nav>
